8a4ef1133023d868e70a1f6618e3cf16afa45414
Observer119 <bgjane@gmail.com>
2025-05-22 21:03:57 +0800

    Fix routing structure and component references

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -37,7 +37,7 @@ const Contact: React.FC = () => {
                     <div>
                       <h4 className="font-semibold mb-1">Telefon</h4>
                       <a href="tel:01711046897" className="text-gray-600 hover:text-primary transition-colors">
-                        0171 / 1046897
+                       +49 171 1144557
                       </a>
                     </div>
                   </div>
@@ -46,8 +46,8 @@ const Contact: React.FC = () => {
                     <Mail className="text-primary flex-shrink-0 mt-1" size={24} />
                     <div>
                       <h4 className="font-semibold mb-1">E-Mail</h4>
-                      <a href="mailto:info@monica-meier.de" className="text-gray-600 hover:text-primary transition-colors">
-                        info@monica-meier.de
+                      <a href="mailto:m.meier@vorsorgewerk24.de" className="text-gray-600 hover:text-primary transition-colors">
+                       m.meier@vorsorgewerk24.de
                       </a>
                     </div>
                   </div>
@@ -57,7 +57,7 @@ const Contact: React.FC = () => {
                     <div>
                       <h4 className="font-semibold mb-1">Standort</h4>
                       <p className="text-gray-600">
-                        Günzburg und Umgebung<br />
+                        München und Umgebung<br />
                         Beratung deutschlandweit verfügbar
                       </p>
                     </div>
@@ -68,7 +68,8 @@ const Contact: React.FC = () => {
                     <div>
                       <h4 className="font-semibold mb-1">Erreichbarkeit</h4>
                       <p className="text-gray-600">
-                        Montag - Freitag: 9:00 - 18:00 Uhr<br />
+                        Montag - Freitag: 9:00 - 20:00 Uhr<br />
+                        Samstag: 9:00 - 15:00 Uhr
                         Oder nach Vereinbarung
                       </p>
                     </div>
@@ -76,18 +77,17 @@ const Contact: React.FC = () => {
                 </div>
               </div>
               
-              {/* Map */}
-              <div className="h-64 bg-gray-300 rounded-lg overflow-hidden">
-                <iframe 
-                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d21386.834583809293!2d10.260775199999999!3d48.45472505!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4799f9a0a1b16a3d%3A0x41f6bb7a5df5f686!2zR8O8bnpidXJn!5e0!3m2!1sde!2sde!4v1624434146583!5m2!1sde!2sde" 
-                  width="100%" 
-                  height="100%" 
-                  style={{ border: 0 }}
-                  allowFullScreen 
-                  loading="lazy"
-                  title="Standort Günzburg"
-                ></iframe>
-              </div>
+                         {/* Map */}
+            <div className="h-64 bg-gray-300 rounded-lg overflow-hidden">
+              <iframe 
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d48892.834583809293!2d11.57549!3d48.13743!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x479e69d9a6c8d8c1%3A0x2fbfcab0d2a9cdee!2sMünchen!5e0!3m2!1sde!2sde!4v1624434146583!5m2!1sde!2sde" 
+                width="100%" 
+                height="100%" 
+                style={{ border: 0 }}
+                allowFullScreen 
+                loading="lazy"
+                title="Standort München"
+              ></iframe>
             </div>
           </div>
         </div>
@@ -104,13 +104,13 @@ const Contact: React.FC = () => {
           </p>
           <div className="flex flex-col sm:flex-row justify-center gap-4">
             <a 
-              href="tel:01711046897" 
+              href="tel:+498920175965" 
               className="btn bg-white text-primary hover:bg-gray-100"
             >
               <Phone size={20} className="mr-2" /> Anrufen
             </a>
             <a 
-              href="https://wa.me/491711046897" 
+              href="https://wa.me/+491711144557" 
               target="_blank" 
               rel="noopener noreferrer"
               className="btn bg-[#25D366] text-white hover:bg-[#128C7E]"
