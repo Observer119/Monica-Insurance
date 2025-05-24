4f6bd3b7c26a9553466c2c43acd4735ceabbc62e
Observer119 <bgjane@gmail.com>
2025-05-24 23:12:22 +0800

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@ function App() {
             <Route index element={<Privatkunden />} />
             <Route path="check" element={<Privatecheck />} />
           </Route>
-          <Route path="business">
+          <Route path="Business">
             <Route index element={<Businesskunden />} />
             <Route path="Businesscheck" element={<Businesscheck />} />
           </Route>
