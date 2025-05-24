f5633153016eeffa84c2852ab53f888eb338be98
Observer119 <bgjane@gmail.com>
2025-05-24 23:05:14 +0800

    Updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ function App() {
         <Route path="Privatkunden" element={<Privatkunden />}>
           <Route path="check" element={<PrivateCheck />} />
       </Route>
-      <Route path="business" element={<Businesskunden />}>
+      <Route path="Business" element={<Businesskunden />}>
       <Route path="check" element={<Businesscheck />} />
           </Route>
         </Route>
