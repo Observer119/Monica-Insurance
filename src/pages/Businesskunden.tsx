31192da4245b3eaf9ddf37b916ae7ae16adf27b5
Observer119 <bgjane@gmail.com>
2025-05-24 23:48:45 +0800

    Updated Businesskunden.tsx

--- src/pages/Businesskunden.tsx
+++ src/pages/Businesskunden.tsx
@@ -115,7 +115,7 @@ const Businesskunden: React.FC = () => {
           </p>
           <div className="mt-8">
             <Link 
-              to="/Business/check" 
+              to="/Businesscheck" 
               className="btn-primary"
             >
               Jetzt ersten Unternehmens-Check durchführen
