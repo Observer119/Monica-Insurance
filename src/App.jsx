cbe27d5d6eda4bbd5a1be156e7a5729e36c4087b
Observer119 <bgjane@gmail.com>
2025-05-25 14:42:34 +0800

    Remove nested Router components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Outlet } from 'react-router-dom';
+import { Routes, Route, Outlet } from 'react-router-dom';
 import Layout from './components/layout/Layout';
 import Home from './pages/Home';
 import About from './pages/About';
@@ -15,33 +15,31 @@ import Privatkunden from './pages/Privatkunden';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="ueber-mich" element={<About />} />
-          <Route path="arbeitsweise" element={<Process />} />
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="ueber-mich" element={<About />} />
+        <Route path="arbeitsweise" element={<Process />} />
 
-          {/* Services Section - Parent Routes */}
-          <Route path="services" element={<Services />} />
-          
-          {/* Privatkunden Parent with Nested Route */}
-          <Route path="services/Privatkunden" element={<Privatkunden />}>
-            <Route path="PrivateCheck" element={<PrivateCheck />} />
-          </Route>
-          
-          {/* Businesskunden Parent with Nested Route */}
-          <Route path="services/Businesskunden" element={<Businesskunden />}>
-            <Route path="BusinessCheck" element={<BusinessCheck />} />
-          </Route>
-
-          {/* Other Pages */}
-          <Route path="kontakt" element={<Contact />} />
-          <Route path="impressum" element={<Impressum />} />
-          <Route path="datenschutz" element={<Datenschutz />} />
+        {/* Services Section - Parent Routes */}
+        <Route path="services" element={<Services />} />
+        
+        {/* Privatkunden Parent with Nested Route */}
+        <Route path="services/Privatkunden" element={<Privatkunden />}>
+          <Route path="PrivateCheck" element={<PrivateCheck />} />
         </Route>
-      </Routes>
-    </Router>
+        
+        {/* Businesskunden Parent with Nested Route */}
+        <Route path="services/Businesskunden" element={<Businesskunden />}>
+          <Route path="BusinessCheck" element={<BusinessCheck />} />
+        </Route>
+
+        {/* Other Pages */}
+        <Route path="kontakt" element={<Contact />} />
+        <Route path="impressum" element={<Impressum />} />
+        <Route path="datenschutz" element={<Datenschutz />} />
+      </Route>
+    </Routes>
   );
 }
 
